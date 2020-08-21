9737c902d39ad5b274769ab8c000bb83354db886
Deepraj Bhosale <deeprajb012@gmail.com>
2020-08-21 20:35:37 +0530

    Add files via upload
    Added phone and tablet responsiveness and aos.

--- main.js
+++ main.js
@@ -3,4 +3,29 @@ $(document).ready(function() {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
     });
+    $('.top-nav .nav-link').on('click', function() {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+    $('nav a[href="#portfolio"]').on('click', function() {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 2000);
+    });
+    $('nav a[href="#about"]').on('click', function() {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top + 130
+        }, 2000);
+    });
+    $('nav a[href="#contact"]').on('click', function() {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top + 130
+        }, 2000);
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1800,
+        once: true
+    });
 });
